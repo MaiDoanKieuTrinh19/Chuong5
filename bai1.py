45e7b8fd659d26bbb261e94e6b6ebe355fe91f2d
MaiDoanKieuTrinh19 <maidoankieutrinh@gmail.com>
2024-09-12 19:21:00 +0700

    Add files via upload

---
+++ bai1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+n=int(input('Nhập số nguyên dương n= '))
+a=1
+for i in range(1,n+1):
+    a=a*i
+print('Giai thừa của số nguyên',n, 'là:',a)
+    
+    
---
+++ bai2.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 12 19:06:54 2024
+
+@author: Student
+"""
+print('======= Bảng cửu chương 2=======')
+for i in range(1,10):
+    hai=i*2
+    print('2*',i,'=',hai)
+print('======= Bảng cửu chương 3=======')
+for i in range(1,10):
+    ba=i*3
+    print('3*',i,'=',ba)
+print('======= Bảng cửu chương 4=======')
+for i in range(1,10):
+    bon=i*4
+    print('4*',i,'=',bon)
+print('======= Bảng cửu chương 5=======')
+for i in range(1,10):
+    nam=i*5
+    print('5*',i,'=',nam)
+print('======= Bảng cửu chương 6=======')
+for i in range(1,10):
+    sau=i*6
+    print('6*',i,'=',sau)
+print('======= Bảng cửu chương 7=======')
+for i in range(1,10):
+    bay=i*7
+    print('7*',i,'=',bay)
+print('======= Bảng cửu chương 8=======')
+for i in range(1,10):
+    tam=i*8
+    print('8*',i,'=',tam)
+print('======= Bảng cửu chương 9=======')
+for i in range(1,10):
+    chin=i*9
+    print('9*',i,'=',chin)
